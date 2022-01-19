af543ac0b20adfb0db6b45f3316ef6bff30a92ea
islanderian <createdby.island@gmail.com>
2022-01-19 23:16:16 +0900

    lec#1.4 - Css Modules

--- componets/NavBar.js
+++ componets/NavBar.js
@@ -1,5 +1,6 @@
 import Link from "next/link"; // <a> 태그 대신 NextJS 에서 네비게이션 이동할 때 사용하는 Router
 import { useRouter } from "next/router";
+import styles from "../styles/NavBar.module.css"; // style 모듈 import
 
 export default function NavBar() {
   const router = useRouter();
@@ -7,10 +8,10 @@ export default function NavBar() {
   return (
     <nav>
       <Link href="/">
-        <a style={{ color: router.pathname === "/" ? "red" : "blue" }}>Home</a>
+        <a className={router.pathname === "/" ? styles.active : ""}>Home</a>
       </Link>
       <Link href="/about">
-        <a style={{ color: router.pathname === "/about" ? "red" : "blue" }}>
+        <a className={router.pathname === "/about" ? styles.active : ""}>
           About
         </a>
       </Link>
