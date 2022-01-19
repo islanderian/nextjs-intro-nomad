5160059b36681311a3f8dad2b307bc04e38b399c
islanderian <createdby.island@gmail.com>
2022-01-19 23:30:07 +0900

    lec#1.5 - Styles JSX

--- componets/NavBar.js
+++ componets/NavBar.js
@@ -1,6 +1,5 @@
 import Link from "next/link"; // <a> 태그 대신 NextJS 에서 네비게이션 이동할 때 사용하는 Router
 import { useRouter } from "next/router";
-import styles from "../styles/NavBar.module.css"; // style 모듈 import
 
 export default function NavBar() {
   const router = useRouter();
@@ -8,13 +7,23 @@ export default function NavBar() {
   return (
     <nav>
       <Link href="/">
-        <a className={router.pathname === "/" ? styles.active : ""}>Home</a>
+        <a className={router.pathname === "/" ? "active" : ""}>Home</a>
       </Link>
       <Link href="/about">
-        <a className={router.pathname === "/about" ? styles.active : ""}>
-          About
-        </a>
+        <a className={router.pathname === "/about" ? "active" : ""}>About</a>
       </Link>
+      {/* Styled JSX */}
+      <style jsx>{`
+        nav {
+          background-color: tomato;
+        }
+        a {
+          text-decoration: none;
+        }
+        .active {
+          color: yellow;
+        }
+      `}</style>
     </nav>
   );
 }
